2944eb52d75dc80bd24bdd331525faf3454dbb49
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2018-05-15 09:21:40 +0530

    Implement main navigation

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import './css/main.css';
 
 import 'jquery';
 
-
+$(".main-nav-box").click(function(){
+    switch ($(this).find("h2").text()){
+        case "O/L":
+            window.location.replace(window.location.origin + "/ordinary-level.html");
+            break;
+        case "A/L":
+            window.location.replace(window.location.origin + "/advanced-level.html");
+            break;
+    }
+});
